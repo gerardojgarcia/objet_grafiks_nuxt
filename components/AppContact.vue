31444cf7e25a562c1e053380cb2cffee83a25dea
Gerardo J Garcia <gerardojgarcia@pm.me>
2023-09-06 02:17:09 -0700

    start contact layout

--- components/AppContact.vue
+++ components/AppContact.vue
@@ -1,18 +1,10 @@
-<script setup>
-
-
-
-</script>
+<script setup></script>
 <template>
-    <section id="contact" class="flex flex-col p-8 bg-gray-200 dark:bg-gray-400  ">
-
-<div>
-    
-
-
-</div>
-
-
-        
-    </section>
+  <section id="contact" class="flex flex-col p-8 bg-gray-200 dark:bg-gray-400">
+    <div
+      class="flex flex-col lg:flex-row md:justify-center md:align-center rounded-xl shadow-lg md:py-16 pb-36 bg-white md:mt-10 dark:bg-night dark:text-white"
+      data-aos="zoom-in-up"
+      data-aos-duration="1000"
+    ></div>
+  </section>
 </template>
